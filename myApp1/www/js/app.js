8554db0275ab75f7352898f00974e6f7b1f6702f
gautham79 <gautham.mallik@gmail.com>
2017-04-24 11:56:39 +0530

    added directive to limit length in text feilds

--- myApp1/www/js/app.js
+++ myApp1/www/js/app.js
@@ -262,7 +262,28 @@ window.AndroidFullScreen.immersiveMode(false, false);
     })
     //$urlRouterProvider.otherwise('/Splash');
 
-}).directive('textarea', function() {
+}).directive('limitChar', function() {
+    'use strict';
+    return {
+        restrict: 'A',
+        scope: {
+            limit: '=limit',
+            ngModel: '=ngModel'
+        },
+        link: function(scope) {
+            scope.$watch('ngModel', function(newValue, oldValue) {
+                if (newValue) {
+                    var length = newValue.toString().length;
+                    if (length > scope.limit) {
+                        scope.ngModel = oldValue;
+                    }
+                }
+            });
+        }
+    };
+})
+
+.directive('textarea', function() {
     return {
         restrict: 'E',
         link: function(scope, element, attr) {
